3aba4038f198a021b880c27c9fff01e0f237bce6
Wanderson Alves Rodrigues <wanderson@pop-os.localdomain>
2021-06-13 13:13:57 -0300

    domain class

---
+++ src/CleanArchitecture.Domain/Entities/Category.cs
@@ -0,0 +1,11 @@
+using System.Collections.Generic;
+
+namespace CleanArchitecture.Domain.Entities
+{
+  public class Category
+  {
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public ICollection<Product> Products { get; set; }
+  }
+}
