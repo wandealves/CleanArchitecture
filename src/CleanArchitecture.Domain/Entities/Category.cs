8a2aff283ac54db8c2b289e15b8e956884cb1c4b
Wanderson Alves Rodrigues <wanderson@pop-os.localdomain>
2021-06-13 13:32:19 -0300

    Enriquecendo o dominio

--- src/CleanArchitecture.Domain/Entities/Category.cs
+++ src/CleanArchitecture.Domain/Entities/Category.cs
@@ -1,11 +1,31 @@
 using System.Collections.Generic;
+using CleanArchitecture.Domain.Validation;
 
 namespace CleanArchitecture.Domain.Entities
 {
-  public class Category
+  public sealed class Category
   {
-    public int Id { get; set; }
-    public string Name { get; set; }
-    public ICollection<Product> Products { get; set; }
+    public Category(string name)
+    {
+      ValidateDomain(name);
+    }
+
+    public Category(int id, string name)
+    {
+      DomainExceptionValidation.When(id < 0, "Inalid Id value");
+      Id = id;
+      ValidateDomain(name);
+    }
+
+    public int Id { get; private set; }
+    public string Name { get; private set; }
+    public ICollection<Product> Products { get; private set; }
+
+    private void ValidateDomain(string name)
+    {
+      DomainExceptionValidation.When(string.IsNullOrEmpty(name), "Inalid name. Name is required");
+      DomainExceptionValidation.When(name.Length < 3, "Invalid name, too short, minimum 3 characteres");
+      Name = name;
+    }
   }
 }
