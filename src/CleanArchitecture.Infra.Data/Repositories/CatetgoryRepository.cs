0e1f7d9fb6c80561dd0793aebe07c54e395a47a2
Wanderson Rodrigues <wanderson.rodrigues@venturisfinancial.com>
2021-06-16 10:03:06 -0300

    criado repositório categoria

---
+++ src/CleanArchitecture.Infra.Data/Repositories/CatetgoryRepository.cs
@@ -0,0 +1,49 @@
+﻿using CleanArchitecture.Domain.Entities;
+using CleanArchitecture.Domain.Interfaces;
+using CleanArchitecture.Infra.Data.Context;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CleanArchitecture.Infra.Data.Repositories
+{
+    public class CatetgoryRepository: ICategoryRepository
+    {
+        private readonly ApplicationDbContext _context;
+        public CatetgoryRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Category> CreateAsync(Category category)
+        {
+            _context.Add(category);
+            await _context.SaveChangesAsync();
+            return category;
+        }
+
+        public async Task<Category> GetByIdAsync(int? id)
+        {
+            return await _context.Categories.FindAsync(id);
+        }
+
+        public async Task<IEnumerable<Category>> GetCategoriesAsync()
+        {
+            return await _context.Categories.ToListAsync();
+        }
+
+        public async Task<Category> RemoveAsync(Category category)
+        {
+            _context.Remove(category);
+            await _context.SaveChangesAsync();
+            return category;
+        }
+
+        public async Task<Category> UpdateAsync(Category category)
+        {
+            _context.Update(category);
+            await _context.SaveChangesAsync();
+            return category;
+        }
+    }
+}
