9df9f5fb2349d85805303ee27e61b77c5a89f6b8
Wanderson Rodrigues <wanderson.rodrigues@venturisfinancial.com>
2021-06-18 09:29:06 -0300

    adição dos dtos

---
+++ src/CleanArchitecture.Application/Mappings/DomainToDTOMappingProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using CleanArchitecture.Application.DTOs;
+using CleanArchitecture.Domain.Entities;
+
+namespace CleanArchitecture.Application.Mappings
+{
+    class DomainToDTOMappingProfile: Profile
+    {
+        public DomainToDTOMappingProfile() 
+        {
+            CreateMap<Category, CategoryDTO>().ReverseMap();
+            CreateMap<Product, ProductDTO>().ReverseMap();
+        }
+    }
+}
