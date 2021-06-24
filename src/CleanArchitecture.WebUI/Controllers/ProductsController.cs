80f4045fb171a4c3e4677ec5746408717fc06158
Wanderson Rodrigues <wanderson.rodrigues@venturisfinancial.com>
2021-06-24 09:24:22 -0300

    foi adicionado o controler de produto

---
+++ src/CleanArchitecture.WebUI/Controllers/ProductsController.cs
@@ -0,0 +1,22 @@
+﻿using CleanArchitecture.Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace CleanArchitecture.WebUI.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly IProductService _productService;
+        public ProductsController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var products = await _productService.GetProducts();
+            return View(products);
+        }
+    }
+}
--- src/CleanArchitecture.WebUI/Startup.cs
+++ src/CleanArchitecture.WebUI/Startup.cs
@@ -47,7 +47,7 @@ namespace CleanArchitecture.WebUI
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Categories}/{action=Index}/{id?}");
+                    pattern: "{controller=Products}/{action=Index}/{id?}");
             });
         }
     }
