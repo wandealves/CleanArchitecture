ce23d5fedbdd73355496f26587078ba37100e193
Wanderson Rodrigues <wanderson.rodrigues@venturisfinancial.com>
2021-06-24 10:12:20 -0300

    adição dos comandos e queries produto

---
+++ src/CleanArchitecture.Application/Products/Queries/GetProductsQuery.cs
@@ -0,0 +1,11 @@
+﻿using CleanArchitecture.Domain.Entities;
+using MediatR;
+using System.Collections.Generic;
+
+namespace CleanArchitecture.Application.Products.Queries
+{
+    public class GetProductsQuery : IRequest<IEnumerable<Product>>
+    {
+  
+    }
+}
