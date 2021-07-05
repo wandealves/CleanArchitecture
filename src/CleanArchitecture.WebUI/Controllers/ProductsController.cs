4e028f260f0e8e2a103b125d308a9f53f348bb90
Wanderson Rodrigues <wanderson.rodrigues@venturisfinancial.com>
2021-07-05 10:00:00 -0300

    adição do editar produto

--- src/CleanArchitecture.WebUI/Controllers/ProductsController.cs
+++ src/CleanArchitecture.WebUI/Controllers/ProductsController.cs
@@ -1,4 +1,5 @@
-﻿using CleanArchitecture.Application.Interfaces;
+﻿using CleanArchitecture.Application.DTOs;
+using CleanArchitecture.Application.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using System.Threading.Tasks;
@@ -27,6 +28,45 @@ namespace CleanArchitecture.WebUI.Controllers
         {
             ViewBag.CategoryId = new SelectList(await _categoryService.GetCategories(), "Id", "Name");
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(ProductDTO productDto)
+        {
+            if (ModelState.IsValid)
+            {
+                await _productService.Add(productDto);
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(productDto);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(int? id) 
+        {
+            if (id == null) return NotFound();
+            var productDto = await _productService.GetById(id);
+
+            if (productDto == null) return NotFound();
+
+            var categories = await _categoryService.GetCategories();
+
+            ViewBag.CategoryId = new SelectList(categories, "Id", "Name", productDto.CategoryId);
+
+            return View(productDto);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(ProductDTO productDto) 
+        {
+            if (ModelState.IsValid) 
+            {
+                await _productService.Update(productDto);
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(productDto);
         }
     }
 }
