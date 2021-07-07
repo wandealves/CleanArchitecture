5bd4e7f18bd0b0229ab1e1bb60746ef9fa85dc7c
Wanderson Rodrigues <wanderson.rodrigues@venturisfinancial.com>
2021-07-07 10:03:37 -0300

    configuração

--- src/CleanArchitecture.Infra.Ioc/DependencyInjection.cs
+++ src/CleanArchitecture.Infra.Ioc/DependencyInjection.cs
@@ -1,10 +1,13 @@
 ﻿using CleanArchitecture.Application.Interfaces;
 using CleanArchitecture.Application.Mappings;
 using CleanArchitecture.Application.Services;
+using CleanArchitecture.Domain.Account;
 using CleanArchitecture.Domain.Interfaces;
 using CleanArchitecture.Infra.Data.Context;
+using CleanArchitecture.Infra.Data.Identity;
 using CleanArchitecture.Infra.Data.Repositories;
 using MediatR;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -19,17 +22,25 @@ namespace CleanArchitecture.Infra.Ioc
             services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"),
                 b => b.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName)));
 
+            services.AddIdentity<ApplicationUser, IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>().AddDefaultTokenProviders();
+
+            services.ConfigureApplicationCookie(options => options.AccessDeniedPath = "/Account/Login");
+
             services.AddScoped<ICategoryRepository, CatetgoryRepository>();
             services.AddScoped<IProductRepository, ProductRepository>();
 
             services.AddScoped<ICategoryService, CategoryService>();
             services.AddScoped<IProductService, ProductService>();
 
+            services.AddScoped<IAuthenticate, AuthenticateService>();
+            services.AddScoped<ISeedUserRoleInitial, SeedUserRoleInitial>();
+
             services.AddAutoMapper(typeof(DomainToDTOMappingProfile));
             services.AddAutoMapper(typeof(DTOToCommandMappingProfile));
 
             var myHandlers = AppDomain.CurrentDomain.Load("CleanArchitecture.Application");
             services.AddMediatR(myHandlers);
+
 
             return services;
         }
